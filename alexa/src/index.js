b464a394018b92b60f43244dcb3a568c0663da7c
Esther Jun Kim <esther.jun.kim@gmail.com>
2016-09-09 21:23:11 -0700

    Update onLaunch speech output

--- alexa/src/index.js
+++ alexa/src/index.js
@@ -44,8 +44,8 @@ ParticleSkill.prototype.eventHandlers.onSessionStarted = function (sessionStarte
 
 ParticleSkill.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
     console.log("ParticleSkill onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
-    var speechOutput = "Welcome to Particle Light. You can ask me to turn on or off. You can also ask me to change colors, like red, green, blue, or white.";
-    var repromptText = "You can ask me to turn on or off. You can also ask me to change colors, like red, green, blue, or white.";
+    var speechOutput = "Welcome to Particle Light. You can ask me to turn the light on or off, change colors, and set the brightness level.";
+    var repromptText = "You can ask me to turn the light on or off.";
     response.ask(speechOutput, repromptText);
 };
 
